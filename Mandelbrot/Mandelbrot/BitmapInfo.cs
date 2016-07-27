9504d9e61cdfaae304274b85a8ea87ea566f52dd
Mayank Metha D <mayankmethad@gmail.com>
2016-07-27 16:58:52 +0530

    BitmapInfo updated

--- Mandelbrot/Mandelbrot/BitmapInfo.cs
+++ Mandelbrot/Mandelbrot/BitmapInfo.cs
@@ -8,5 +8,14 @@ namespace Mandelbrot
 {
     class BitmapInfo
     {
+        public BitmapInfo(int pixelWidth, int pixelHeight, int[] iterationCounts)
+        {
+            PixelWidth = pixelWidth;
+            PixelHeight = pixelHeight;
+            IterationCounts = iterationCounts;
+        }
+        public int PixelWidth { private set; get; }
+        public int PixelHeight { private set; get; }
+        public int[] IterationCounts { private set; get; }
     }
 }
